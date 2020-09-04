431853021c9ef95d8880121a0f24287a0d1a26a0
Adarsh <shrivastava.adarsh27@gmail.com>
2020-09-04 15:56:55 +0530

    Updates

---
+++ precache-manifest.6214eee3bce8a2647382368788056062.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a2fc0294ff6720c9d8dc8043302c76",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "74bdcf7375caadb2efa5",
+    "url": "/reactwebsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "5b0b98d21df77084697c",
+    "url": "/reactwebsite/static/css/main.f84bdf1d.chunk.css"
+  },
+  {
+    "revision": "74bdcf7375caadb2efa5",
+    "url": "/reactwebsite/static/js/2.a91e289b.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactwebsite/static/js/2.a91e289b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0b98d21df77084697c",
+    "url": "/reactwebsite/static/js/main.ed103392.chunk.js"
+  },
+  {
+    "revision": "08c16195d48d7d74fbfe",
+    "url": "/reactwebsite/static/js/runtime-main.99174728.js"
+  },
+  {
+    "revision": "db27a4be2a746319eb915aebddbd1e19",
+    "url": "/reactwebsite/static/media/app.db27a4be.jpg"
+  },
+  {
+    "revision": "90412cf3030fffab439916647b545786",
+    "url": "/reactwebsite/static/media/digitalmarketing.90412cf3.jpg"
+  },
+  {
+    "revision": "267c1d9aa6009db43aad0c63f2d4e7b1",
+    "url": "/reactwebsite/static/media/home.267c1d9a.png"
+  },
+  {
+    "revision": "7f2407abc8e84db3b76d5876552d8bb5",
+    "url": "/reactwebsite/static/media/img2.7f2407ab.jpg"
+  },
+  {
+    "revision": "3f51d31828548ab3ae9029be4692ee12",
+    "url": "/reactwebsite/static/media/marketing.3f51d318.jpg"
+  },
+  {
+    "revision": "2f7df8b58e3441b229e8261d8b278e57",
+    "url": "/reactwebsite/static/media/software.2f7df8b5.jpg"
+  },
+  {
+    "revision": "55aa4d9e218ac9fff6ca82939c0c1846",
+    "url": "/reactwebsite/static/media/web.55aa4d9e.jpg"
+  }
+]);
